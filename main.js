af8c54794b4c1a768ac7ca936a3be92c203d9b45
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-08-08 22:43:34 +0300

    Add function prepareCurrentSlidex.

--- main.js
+++ main.js
@@ -29,22 +29,26 @@ const activeDot = n => {
     dots[n].classList.add('active');
 }
 
+// ф-ція, яка об'єднує виклик двох ф-цій (для чистоти кода)
+const prepareCurrentSlide = ind => {
+    // оновити слайд на екрані
+    activeSlide(index);
+    // оновити dot на екрані
+    activeDot(index);
+}
+
 // функція для перемикання на наступний слайд
 const nextSlide = () => {
     // перевірка на останній слайд; 'якщо слайд останній, то наступний слайд буде з індексом 0'
     if (index == slides.length - 1) {
         index = 0;
-        // оновити слайд на екрані
-        activeSlide(index);
-        // оновити dot на екрані
-        activeDot(index);
+        // оновити слайд та dot на екрані
+        prepareCurrentSlide(index);
     } else {
         // index збільшую на 1
         index++;
-        // оновити слайд на екрані
-        activeSlide(index);
-        // оновити dot на екрані
-        activeDot(index);
+        // оновити слайд та dot на екрані
+        prepareCurrentSlide(index);
     }
 };
 
@@ -53,17 +57,13 @@ const prevSlide = () => {
     // перевірка на перший слайд; якщо слайд перший, то наступний слайд буде з максимальним індексом
     if (index == 0) {
         index = slides.length - 1;
-        // оновити слайд на екрані
-        activeSlide(index);
-        // оновити dot на екрані
-        activeDot(index);
+        // оновити слайд та dot на екрані
+        prepareCurrentSlide(index);
     } else {
         // index зменшую на 1
         index--;
-        // оновити слайд на екрані
-        activeSlide(index);
-        // оновити dot на екрані
-        activeDot(index);
+        // оновити слайд та dot на екрані
+        prepareCurrentSlide(index);
     }
 }
 
